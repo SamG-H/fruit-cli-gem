90ac81bf20aca4389b017f0404bd4d24307b2cbb
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-07-07 18:01:29 -0700

    more colorize

--- lib/services/cli.rb
+++ lib/services/cli.rb
@@ -8,39 +8,64 @@ class CLI
       puts "Press a to see all fruit"
       puts "Press s to see all fruit with their scientific names"
       puts "Press n to see all fruit with their nutrional information"
-      puts "Type the name of a fruit to see all of its information"
-      puts ""
-      puts "What would you like to do:"
+      puts "Type the name of a fruit to see all of its information\n"
+      print "What would you like to do: "
       input = gets.strip.downcase
       case input
       when 'q'
         return
       when 'a'
-        puts ""
         display_all_fruit
-        puts ""
+      when 's'
+        display_fruits_with_scientific_names
+      when 'n'
+        display_nutrional_info
       else
-        puts "\nNot a valid option or fruit\n".red
+        if Fruit.all.find { |fruit| fruit.name.downcase == input} != nil
+          display_one_fruit(Fruit.all.find { |fruit| fruit.name.downcase == input})
+        else
+          puts "\nNot a valid option or fruit\n".red
+        end
       end
-      # fruit_name = gets.strip
-      # display_nutrional_info(fruit_name)
     end
+  end
+
+  def display_one_fruit(fruit)
+    puts "\n#{fruit.name} - #{fruit.order} #{fruit.family} #{fruit.genus}".colorize(fruit.color)
+    fruit.nutritions.each do |k, v| 
+      if k == "calories" 
+        puts "#{k}: #{v}".colorize(:color => :black, :background => fruit.color)
+      else
+        puts "#{k}: #{v}g".colorize(:color => :black, :background => fruit.color)
+      end
+    end
+    puts ""
   end
 
   def display_all_fruit
+    puts ""
     Fruit.all.each { |fruit| puts "#{fruit.name}".colorize(fruit.color) }
+    puts ""
   end
 
-  def display_nutrional_info(fruit_name)
-    the_fruit = Fruit.all.find { |fruit| fruit.name == fruit_name}
+  def display_nutrional_info
     puts ""
-    puts "#{the_fruit.name} per 100g" 
-    the_fruit.nutritions.each do |k, v| 
-      if k == "calories" 
-        puts "#{k}: #{v}"
-      else
-        puts "#{k}: #{v}g"
+    Fruit.all.each do |fruit|
+      puts "#{fruit.name}".colorize(fruit.color) + " per 100g" 
+      fruit.nutritions.each do |k, v| 
+        if k == "calories" 
+          puts "#{k}: #{v}".colorize(:color => :black, :background => fruit.color)
+        else
+          puts "#{k}: #{v}g".colorize(:color => :black, :background => fruit.color)
+        end
       end
+      puts ""
     end
+  end
+
+  def display_fruits_with_scientific_names
+    puts ""
+    Fruit.all.each { |fruit| puts "#{fruit.name}: #{fruit.order} #{fruit.family} #{fruit.genus}".colorize(fruit.color) }
+    puts ""
   end
 end
