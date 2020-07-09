8f0cebbd96cdf7363759e832209454e6b11bf901
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-07-09 10:43:55 -0700

    put all classes in the FruitInfo module

--- lib/fruit.rb
+++ lib/fruit.rb
@@ -1,61 +1,63 @@
 # frozen_string_literal: false
 
 # a class to hold the fruits information inculding a hash of nutritional facts
-class Fruit
-  attr_accessor :genus, :name, :id, :family, :order, :nutritions, :color
-  @@all = []
-  def initialize(attributes)
-    attributes.each { |k, v| send("#{k}=", v) }
-    add_color
-    @@all << self
-  end
-
-  def self.max(element)
-    all.max { |a, b| a.nutritions[element] <=> b.nutritions[element] }
-  end
-
-  def self.min(element)
-    all.min { |a, b| a.nutritions[element] <=> b.nutritions[element] }
-  end
-
-  def add_color
-    case name
-    when 'Banana'
-      self.color = :light_yellow
-    when 'Blueberry'
-      self.color = :blue
-    when 'Cherry'
-      self.color = :red
-    when 'Apple'
-      self.color = :green
-    when 'Lemon'
-      self.color = :light_yellow
-    when 'Mango'
-      self.color = :yellow
-    when 'Orange'
-      self.color = :yellow
-    when 'Pear'
-      self.color = :green
-    when 'Pineapple'
-      self.color = :light_yellow
-    when 'Raspberry'
-      self.color = :light_red
-    when 'Strawberry'
-      self.color = :red
-    when 'Tomato'
-      self.color = :red
-    when 'Watermelon'
-      self.color = :light_green
-    else
-      self.color = :white
+module FruitInfo
+  class Fruit
+    attr_accessor :genus, :name, :id, :family, :order, :nutritions, :color
+    @@all = []
+    def initialize(attributes)
+      attributes.each { |k, v| send("#{k}=", v) }
+      add_color
+      @@all << self
     end
-  end
 
-  def self.all
-    @@all
-  end
+    def self.max(element)
+      all.max { |a, b| a.nutritions[element] <=> b.nutritions[element] }
+    end
 
-  def self.new_from_api(fruits)
-    fruits.each { |fruit| new(fruit) }
+    def self.min(element)
+      all.min { |a, b| a.nutritions[element] <=> b.nutritions[element] }
+    end
+
+    def add_color
+      case name
+      when 'Banana'
+        self.color = :light_yellow
+      when 'Blueberry'
+        self.color = :blue
+      when 'Cherry'
+        self.color = :red
+      when 'Apple'
+        self.color = :green
+      when 'Lemon'
+        self.color = :light_yellow
+      when 'Mango'
+        self.color = :yellow
+      when 'Orange'
+        self.color = :yellow
+      when 'Pear'
+        self.color = :green
+      when 'Pineapple'
+        self.color = :light_yellow
+      when 'Raspberry'
+        self.color = :light_red
+      when 'Strawberry'
+        self.color = :red
+      when 'Tomato'
+        self.color = :red
+      when 'Watermelon'
+        self.color = :light_green
+      else
+        self.color = :white
+      end
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.new_from_api(fruits)
+      fruits.each { |fruit| new(fruit) }
+    end
   end
 end
--- lib/fruityvice_api.rb
+++ lib/fruityvice_api.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: false
 
 # accesses the fruityvice api, parses it, and calls Fruit class to make fruit from parsed hash
-class FruityviceAPI
-  BASE_URL = 'https://www.fruityvice.com/api/fruit/all'
+module FruitInfo
+  class FruityviceAPI
+    BASE_URL = 'https://www.fruityvice.com/api/fruit/all'
 
-  def access_fruityviceapi
-    JSON.parse(HTTParty.get(BASE_URL, verify: false))
-  end
+    def access_fruityviceapi
+      JSON.parse(HTTParty.get(BASE_URL, verify: false))
+    end
 
-  def make_fruits
-    Fruit.new_from_api(access_fruityviceapi)
+    def make_fruits
+      Fruit.new_from_api(access_fruityviceapi)
+    end
   end
 end
